3192cb943a010bbdb3c3f8f018f15b2b824bdf9d
Gandalf <gendalph945@gmail.com>
2024-02-12 18:41:32 +0700

    Switching machines

--- shapes.cpp
+++ shapes.cpp
@@ -7,38 +7,6 @@
 
 float bar_scale;
 
-TextBox::TextBox(float pos_x, float pos_y, float size_x, float size_y) {
-
-    m_posx2 = pos_x + size_x;
-    m_posy2 = pos_y + size_y;
-    
-    m_body.setPosition(sf::Vector2f(pos_x, pos_y));
-    m_body.setSize(sf::Vector2f(size_x, size_y));
-};
-
-void TextBox::drawTo(sf::RenderWindow &window) {
-    
-    window.draw(m_body);
-};
-
-void TextBox::selectForm(sf::Vector2i mousePos) {
-    
-    if (m_body.getGlobalBounds().contains((float)mousePos.x, (float)mousePos.y)) {
-        std::cout << "Mouse inside\n";
-        m_isSelected = true;
-        m_body.setFillColor(sf::Color::Green);
-    }
-    else {
-        std::cout << "Mouse outside\n";
-        m_isSelected = false;
-        m_body.setFillColor(sf::Color::White);
-    };
-};
-
-void typeIn(int simbol) {
-    std::cout << simbol;
-};
-
 // ===================================
 
 Bar::Bar(int value, sf::Color color, bool isFirst) {
@@ -69,7 +37,7 @@ void Bar::drawBar(sf::RenderWindow& window) {
     }
 
     sf::Font dos;
-    dos.loadFromFile("DOSfont.ttf");
+    dos.loadFromFile("../fonts/dos.ttf");
 
     _barValue.setCharacterSize(30);
     _barValue.setOutlineColor(sf::Color::White);
--- shapes.hpp
+++ shapes.hpp
@@ -1,30 +1,8 @@
 #pragma once
 
 #include <SFML/Graphics.hpp>
-#include <iostream>
-#include <sstream>
-
 
 void draw_grid(sf::RenderWindow& window);
-
-
-class TextBox {
-public:
-	
-	TextBox(float pos_x, float pos_y, float size_x, float sixe_y);
-    void drawTo(sf::RenderWindow& window);
-    void selectForm(sf::Vector2i mousePos);
-    void typeIn(int simbol);
-
-private:
-	
-	sf::RectangleShape m_body;
-    bool m_isSelected = false;
-
-    float m_posx2;
-    float m_posy2;
-};
-
 
 class Bar {
 public:
